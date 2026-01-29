56ce1b8afcf79937c934daa69bc936143bcdb623
knowledgesolutionstecnology <knowledgesolutionstecnology@gmail.com>
2026-01-29 14:21:01 -0300

    commit inicial

---
+++ src/main/java/br/com/knowledge/knowledgebeer/KnowledgebeerApplication.java
@@ -0,0 +1,13 @@
+package br.com.knowledge.knowledgebeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgebeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgebeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/knowledge/knowledgebeer/KnowledgebeerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.knowledge.knowledgebeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgebeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
